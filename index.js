2f722dac40504c20504f653b217fdbd339887140
MMcClure11 <mcclme02@luther.edu>
2020-09-02 11:57:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = 14 % 5;
+
+let max = Math.max( 2, 5, 9, 20, 16);
