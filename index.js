29cd8ac0be172a7eb5b69c7bbce5202d9032a51d
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-13 20:13:37 +0000

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 //Create a variable called mod set to an equation whose remainder is equal to "4".
 //Create a variable called max that will return "20" as the highest number in the set.
 
-var num1=31
-var num2=2
+const num1=31
+const num2=2
  const multiply =num1 * num2
 
  const random = Math.random()
