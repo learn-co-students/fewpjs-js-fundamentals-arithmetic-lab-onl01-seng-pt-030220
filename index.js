74daad16b5096925507f68257eb27863309d9be7
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-09-17 12:57:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let set = (1, 7, 9, 12, 14, 17, 20)
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
+
+const random = Math.random()
+const mod = (num1 % 9)
+const max = Math.max(set)
